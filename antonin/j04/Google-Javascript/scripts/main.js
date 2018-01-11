50a2265d9891c6a0837d77f903b2aa3c2ede9d1a
Antonin <antonin.siguierfr@gmail.com>
2018-01-11 23:57:07 +0100

    Toujours plus de commits

---
+++ antonin/j04/Google-Javascript/scripts/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("javascript-button").style.display = "none";
+    document.getElementById("javascript-test").style.display = "block";
+}
+
+setTimeout(function(){
+ 	alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+ }, 10000);
